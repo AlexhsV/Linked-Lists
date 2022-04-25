d9a967f0a72806ac92a33a720adb1642dbdb167a
AlexhsV <98179885+AlexhsV@users.noreply.github.com>
2022-04-25 12:35:00 +0300

    added the making of L list

--- Lists.hpp
+++ Lists.hpp
@@ -1,11 +1,61 @@
 #include <iostream>
-#include "Lists.h"
 using namespace std;
 
+template<class T>
+class Chain;
 
 
-
+template<class T>
+class ChainNode{
+    friend Chain<T>;
+    
+    private:
+        T data;
+        ChainNode<T> *link;
+};
 template <class T>
+class Chain{
+    
+    public:
+        Chain(){first = 0;}   // constructor
+        ~Chain() ;  // destructor
+
+        bool IsEmpty() const {return first == 0;}
+        int Length() const ;
+
+        // return the k'th element of list 
+        int Find(int k) const;
+        // return true if k'th exist and put it in x. return false otherwise
+        bool Find_List(int k, T& x) const;
+        
+
+        // return position of x
+        int Search(const T& x) const;
+        
+        Chain<T>& Append(const T& x);
+
+        // delete all apearances of k'th element 
+        
+
+        // insert x just after k'th element
+        Chain<T>& Insert(int k, const T& x);
+        //Delete k'th element 
+        Chain<T>& Delete(int k);
+        Chain<T>& Replace(int k, const T& x);
+        
+        void Swap(int i,int j);//swaps 2 elements of the given chain
+        void Output(ostream& out) const;
+    private:
+        ChainNode<T> *first;//pointer to first node
+        ChainNode<T> *last;//pointer to last node
+
+};
+
+
+
+
+
+template<class T>
 Chain<T> :: ~Chain()
 {//Chain destructor. Delete all nodes in chain
     ChainNode<T> *next;//next node
@@ -258,4 +308,3 @@ ostream& operator<<(ostream& out,const Chain<T>& x)
     x.Output(out);
     return out;
 }
-
