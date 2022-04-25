98ffba4315f95661e23d8644dcf99222459d33a6
AlexhsV <98179885+AlexhsV@users.noreply.github.com>
2022-04-25 12:34:28 +0300

    added the making of L list

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
-#include "Lists.h"
+#include "Lists.hpp"
 #include <iostream>
+#include <random>
+#include <functional>
 using namespace std;
 
 int main() {
+    
+    Chain<int> L;
+    
     int n;
     do {
         cout << "Enter the size of the list, named 'L': ";
@@ -11,18 +16,25 @@ int main() {
             cout << "Invalid size, please try again!\n" << "TIP: (the size of the list must be an integer greater than 0)\n \n";
         }
     } while (n <= 0);
-    cout << n;
 
-    Chain<int> L;
+    default_random_engine generator;
+    uniform_int_distribution<int> data_element_distribution(0, 100);
+    auto random_element = bind(data_element_distribution, generator);
+   
+    
+    for (int i=0; i < n; i++){
+      int data_element = random_element();
+   //   cout << data_element << endl ;
+      L.Append(data_element);
+    }
+
 
     cout << "Length = " << L.Length() << endl;
 
+    
 
-  /*  default_random_engine generator;
-    uniform_int_distribution<int> data_element_distribution(0, 100);
-    auto random_element = bind(data_element_distribution, generator);
-    int data_element = random_element();
-  */
+    
+  
 
 
 
