04b82ff8e04801812491a869ef659b312172310e
AlexhsV <98179885+AlexhsV@users.noreply.github.com>
2022-05-17 14:04:20 +0300

    final project

--- Lists.hpp
+++ Lists.hpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+
+/*In this project we excluded the rest of the methods which are used for linked list functionality because this specific
+purpose we did not need them*/
+
 template<class T>
 class Chain;
 
@@ -8,47 +12,32 @@ class Chain;
 template<class T>
 class ChainNode{
     friend Chain<T>;
-    
-    private:
-        T data;
-        ChainNode<T> *link;
+
+private:
+    T data;
+    T data2;
+    ChainNode<T> *link;
 };
 template <class T>
 class Chain{
-    
-    public:
-        Chain(){first = 0;}   // constructor
-        ~Chain() ;  // destructor
 
-        bool IsEmpty() const {return first == 0;}
-        int Length() const ;
+public:
+    Chain(){first = 0;}   // constructor
+    ~Chain() ;  // destructor
 
-        // return the k'th element of list 
-        int Find(int k) const;
-        // return true if k'th exist and put it in x. return false otherwise
-        bool Find_List(int k, T& x) const;
-        
+    Chain<T>& Append(const T& x);
+    Chain<T>& AppendH(const T& x, const T& b);
+    int countInt(const T& x) const;
 
-        // return position of x
-        int Search(const T& x) const;
-        
-        Chain<T>& Append(const T& x);
+    void Output(ostream& out) const;
 
-        // delete all apearances of k'th element 
-        
+    void OutputH(ostream& out) const;
 
-        // insert x just after k'th element
-        Chain<T>& Insert(int k, const T& x);
-        //Delete k'th element 
-        Chain<T>& Delete(int k);
-        Chain<T>& Replace(int k, const T& x);
-        
-        void Swap(int i,int j);//swaps 2 elements of the given chain
-        void Output(ostream& out) const;
-        void EachNode(int n) const;
-    private:
-        ChainNode<T> *first;//pointer to first node
-        ChainNode<T> *last;//pointer to last node
+
+
+private:
+    ChainNode<T> *first;//pointer to first node
+    ChainNode<T> *last;//pointer to last node
 
 };
 
@@ -69,175 +58,7 @@ Chain<T> :: ~Chain()
 
 }
 
-template<class T>
-int Chain<T>::Length() const
-{// Return the number of elements in the chain
-    ChainNode<T> *current = first;
-    int len = 0;
-    while(current)
-    {
-        len++;
-        current=  current->link;
-    }
-    return len;
-
-}
-
-template<class T>
-int Chain<T>::Find(int k) const
-{// Set x to k'th element in the chain.
- //Return -1 if no k'th; return x otherwise.
-    if(k<1) 
-        throw out_of_range("Invalid element location in Find");// no k'th
-    ChainNode<T> *current = first;
-    int index = 1;//index of current
-    while (current && index < k )
-    {
-        current = current->link;
-        index++;
-        
-    }
-     
-    if(current){    
-       int x = current->data;
-        return x;
-    }
-
-    return -1;    
-     
-}
-
-template<class T>
-bool Chain<T>::Find_List(int k, T& x) const
-{// Set x to the k'th element in the chain.
-    // Return false if no k'th; return true otherwise.
-    if (k < 1) return false;
-        ChainNode<T> *current = first;
-    int index = 1; // index of current
-    while (index < k && current) 
-    {
-        current = current->link;
-        index++;
-    }
-    if (current)
-     {
-        x = current->data;
-        return true;
-    }
-    return false; // no k'th element
-}
-
-
-template<class T>
-int Chain<T>::Search(const T& x) const
-{//Locate x.Return position of x if found
- //return 0 if x not in the chain.
-    ChainNode<T> *current = first;
-    int index = 1;//index of current
-    while (current && current->data !=x)
-    {
-        current = current->link;
-        index++;
-    }
-    if(current)
-        return index;
-    return 0;   
-}
-
-
-template<class T>
-Chain<T>& Chain<T>::Delete(int k)
-{// Set x to the k'th element and delete it.
-// throw outofbounds exception if no k'th element.
-    if (k < 1 || !first )
-        throw out_of_range("Invalid element location in Delete");// no k'th
-
-    // p will eventually point to k'th node
-    ChainNode<T> *p = first;
-
-    //move p to k'th &remove from chain
-    if(k==1)//p already at k'th
-        first = first->link; // remove 
-    else
-    {
-        ChainNode<T> *q = first;
-        for (int index =1; index< k-1 && q;index++)
-        {
-            q=q->link;
-        }    
-        if(!q || !q->link)
-            throw out_of_range("Invalid element location in Delete");//no k'th
-        p = q->link;//k'th
-        if(p==last)
-            last = q;
-        q->link = p->link;//remove from chain
-
-    }
-    
-    delete p;
-    return *this;       
-
-}
-
-
-template<class T>
-Chain<T>& Chain<T>::Insert(int k, const T& x)
-{//Insert x after k'th.
-//throw exception if no k'th.
-//throw exception if no space.
-    if (k<0)
-        throw length_error("No space in list to add new element in Insert");
-    //p will eventually point to k'th
-    ChainNode<T> *p = first;
-    for (int index= 1;index <k && p; index++)
-    {//move p to k'th
-        p = p->link;
-    }    
-    if (k > 0 && !p)
-        throw out_of_range("Invalid element location in Insert");
-
-    //insert
-    ChainNode<T> *y = new ChainNode<T>;
-    y->data = x;
-    if (k)
-    {//Insert after p
-        y->link = p->link;
-        p->link = y;
-
-    }else
-    {//insert as first element
-        y->link = first;
-        first = y;
-
-    }
-        
-    if(!y->link)
-        last =y;//new last element    
-    return *this;
-
-}
-
-template<class T>
-Chain<T>& Chain<T>::Replace(int k, const T& x)
-{//Insert  k'th with x.
-//throw exception if no k'th.
-
-    
-    //p will eventually point to k'th
-    ChainNode<T> *p = first;
-    for (int index= 1;index <k && p; index++)
-    {//move p to k'th
-        p = p->link;
-    }    
-    if (k > 0 && !p)
-        throw out_of_range("Invalid element location in Replace");
-
-    p->data =x;
-    return *this;
-
-}
-
-
+//Append for L list
 template <class T>
 Chain<T>& Chain<T>::Append(const T& x)
 {// Add x at right end.
@@ -258,34 +79,30 @@ Chain<T>& Chain<T>::Append(const T& x)
 
 }
 
-
+// Append for histogram list (two elements in each node)
 template <class T>
-void Chain<T>::Swap(int i , int j)
-{
-    //swaps i with j.
-   int temp;
-   ChainNode<T> *currentlink = first;
-   ChainNode<T> *currentlink2=first;
-   int index =1;
-   while(currentlink && index<j)
-   {
-       currentlink=currentlink->link;
-       index+=1;//moving index to j
-   }
-   temp=currentlink->data;//saving data of j
-   
-   index=1;
-   while(currentlink2 && index<i)
-   {
-       currentlink2=currentlink2->link;
-        index+=1;//moving index to i.
-   }
-   currentlink->data=currentlink2->data;//puts i's data to j's
-   currentlink2->data=temp;//puts temp's(j's)data to i's.
+Chain<T>& Chain<T>::AppendH(const T& x, const T& b)       //b is the frequency
+{// Add x at right end
+    // Add b at right end
+    ChainNode<T> *y;
+    y= new ChainNode<T>;
+    y->data = x;
+    y ->data2 = b;
+    y->link = 0;
+    if(first)
+    {//chain is not empty
+        last->link =y;
+        last =y;
+
+    }
+    else // chain is empty
+        first = last = y;
+
+    return *this;
 
 }
 
-
+//Output for L list
 template<class T>
 void Chain<T>::Output(ostream& out) const
 {//Insert the chain elements into the stream out.
@@ -304,4 +121,34 @@ ostream& operator<<(ostream& out,const Chain<T>& x)
 {
     x.Output(out);
     return out;
+}
+
+//Count the frequency of each element
+template<class T>
+int Chain<T>::countInt(const T& x) const {
+    ChainNode<T>* current = first;
+    int intCount = 0;
+    while (current && (current->link!=NULL)) {   //check if current is pointing and a node and if the link of the node is different from NULL
+        if (current->data == x)   //checking if the data in the node is the same as the element we passed as an argument
+            intCount++;
+        current = current->link;
+    }
+    if(current->link==NULL && (current->data==x))  //check for data in last node
+    {
+        intCount++;
+    }
+    return intCount;
+}
+
+//Output for histogram list(two elements in each node)
+template<class T>
+void Chain<T>::OutputH(ostream& out) const
+{//Insert the chain elements into the stream out.
+    ChainNode<T> *current;
+
+    for(current = first;current;current = current->link){
+        cout <<"data: "<<current->data<<"  ";
+        cout <<"frequency of data: "<<current->data2<<"  ";
+        cout<<endl;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -4,98 +4,42 @@
 #include <functional>
 using namespace std;
 
-
-void frequency_histogram(int ar[], int size){  /*function that calculates the frequency of the ar's elements that
-                                                 includes the lists's (L) elements*/
-                                                 
-  Chain<int> histogram;  //creation of histogram list
-
-  bool* check = new bool[size];  //create a topic dynamic array (check) that helps to find the frequency od the list's elements
-
-  for(int i=0;i<size;i++){   //initialize the dynamic array with false state
-    check[i] = false;
-  }
-
-   for(int i=0; i<size; i++){  
-      if(check[i]== true){
-         continue;
-      }
-      int count = 1;                     //create a counter that inlcludes the frequency of every element in array(ar)
-      for(int j = i+1; j<size; j++){    
-                                          
-         if (ar[i] == ar[j]){           //check if the element of array(ar) is equal with next one
-            check[j] = true;           //set true to array (check) when the condition the equality is true
-            count++;                  //increase count
-
-         }
-      }
-
-  //    cout<<"frequency of "<<ar[i]<<" is: " << count << endl;
-
-      histogram.Append(count);   //import count in new list histogram
-   
-   }
-
-    cout << "The elements of the 'histogram' list are: ";
-    histogram.Output(cout);   //print histogram's elements
-
-    delete [] check;       //delete the dynamic array(check)
-}
-
-
-void bubble_sort(int ar[], int n){  //function that sorts the elements of array (ar)  
-  for(int i=0; i<n-1; i++){
-    for(int j=0; j<n-1; j++){
-      if(ar[j]>ar[j+1]){
-         swap(ar[j], ar[j + 1]);
-      }
-    }
-  }
-}
-
-
 int main() {
-    
-    Chain<int> L;
-    
-    int n; 
-    do {
-        cout << "Enter the size of the list, named 'L': ";
-        cin >> n;
-        if (n <= 0){
-            cout << "Invalid size, please try again!\n" << "TIP: (the size of the list must be an integer greater than 0)\n \n";
-        }
-    } while (n <= 0);
 
-    default_random_engine generator;
-    uniform_int_distribution<int> data_element_distribution(0, 100);
-    auto random_element = bind(data_element_distribution, generator);
-   
-    int* ar = new int[n]; //create dynamic array 
-    for (int i=0; i < n; i++){                         
-      int data_element = random_element();       
-      L.Append(data_element);                   //import elements on L 
-      ar[i]=data_element;                      //import the same elements on ar
-    }
+   Chain<int> L;
+   Chain<int> histogram;
+   int n;
+   cout << "Enter the size of the list 'L':  ";
+   cin >> n;
+   while (n <= 0){
+      cout << "Invalid size, please try again!\n" << "TIP: (the size of the list must be an integer greater than 0): ";
+      cin >> n;
+      cout << endl;     }
 
-    cout<<"\nThe elements of the 'L' list are: ";
-    L.Output(cout);
-    cout<<endl;
-    cout<<endl;
-    
-    bubble_sort(ar, n);                       //call the bubble_sort function
-    
-    cout<<"\nThe elements of the 'L' list sorted are: ";
-    
-    int i;
-    for (i = 0; i < n; i++)
-        cout << ar[i] << " ";  //print the list's L elements through ar array
-    cout << endl;
+   default_random_engine generator;
+   uniform_int_distribution<int> data_element_distribution(0, 100);
+   auto random_element = bind(data_element_distribution, generator);
 
-    frequency_histogram(ar, n); //call frequency_histogram function
-   
-   
-    delete [] ar; //delete the dynamic array (ar)
+   for (int i = 0; i < n; i++) {        //insert at the right end random elements to L list
+      int data_element = random_element();
+      L.Append(data_element);
+   }
+   cout << "\nThe elements of the 'L' list are: ";  //print elements of L list using Output
+   L.Output(cout);
+   cout << endl;
+   cout << endl;
 
-    return 0;
- }
+   //checking if numbers within the limit (0,100) are in L
+   for (int i = 0; i < 100; i++) {
+      int a = L.countInt(i);      //'a' stores the multitude of each number
+      if(a != 0)                     //checking if the number (i) is in L then 'a' will be different from zero
+      {
+         histogram.AppendH(i,a);
+      }
+
+   }
+   cout<<"The elements of the 'histogram' list are: \""<<endl;
+   histogram.OutputH(cout);
+
+   return 0;
+}
